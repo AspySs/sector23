d79f43c0308c99fe6bd027e0e14fcb8cae3ad454
Aspy <52931655+AspySs@users.noreply.github.com>
2019-09-08 22:41:33 +0300

    complete schetchik!

--- index.php
+++ index.php
@@ -1,25 +1,36 @@
 <?php
+$date = time();
+$ip = getIP();
 require "includes/bd.php";
-$bd -> query("INSERT"); // DODELAT
+$bd -> query("INSERT INTO `users` (`id`, `ip`, `time`) VALUES (NULL, '".$ip."', '".$date."');");
 $ALLusers = $bd -> query("SELECT MAX(`id`) FROM `users`");
-$COLVO = vivCOL($ALLusers);
 $bd -> close();
-
-
-
-
-
-
+$COLVO = vivCOL($ALLusers);
+$numb = 7000+$COLVO;
 
 
 function vivCOL($result_set){
 
-	while(($row = $result_set->fetch_assoc()) != false){		
-	}
-		$numb = $result_set->num_rows;
-		return $numb;
-}
+	while(($row = $result_set->fetch_assoc()) != false){
 
+		//echo $row["login"];
+		//echo "<br />";
+		return $row["MAX(`id`)"];
+		
+	}
+}
+function getIP()
+{
+	$client  = @$_SERVER['HTTP_CLIENT_IP'];
+	$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+	$remote  = @$_SERVER['REMOTE_ADDR'];
+ 
+	if(filter_var($client, FILTER_VALIDATE_IP)) $ip = $client;
+	elseif(filter_var($forward, FILTER_VALIDATE_IP)) $ip = $forward;
+	else $ip = $remote;
+ 
+		return $ip;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,6 +40,7 @@ function vivCOL($result_set){
 	<title>Sector 23</title>
 </head>
 <body>
+	<?php echo "количество посетителей: ".$numb; ?>
 	<header>
 		<div class="left"><span>Sector 23<br> Лазаревское</span></div>
 	</header>
